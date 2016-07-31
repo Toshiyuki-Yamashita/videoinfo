a55d5407f410d5e31f55b2c0300a56996150c239
Toshiyuki-Yamashita <yamasita@buffie.jp>
2016-07-31 23:51:18 +0900

    print frame count

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,22 @@
 
 int main(int argc, char** argv)
 {
+	char* input = NULL;
+	if (argc > 1)
+	{
+		input = argv[1];
+	}
 	cv::VideoCapture cap;
 
-	cap.open("test.avi");
+	if (input == NULL)
+	{
+		return -1;
+	}
+	cap.open(input);
+
+	int frames = static_cast<int>(cap.get(CV_CAP_PROP_FRAME_COUNT));
+
+	fprintf(stdout, "%ld\n", frames);
+
 	return 0;
 }
